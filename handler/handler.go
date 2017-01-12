63b06dba5d6a872d13886735248b10486e2ef213
Fumihiro Itoh <fmhrit@gmail.com>
2017-01-12 22:19:25 +0900

    add Hear

--- handler/handler.go
+++ handler/handler.go
@@ -13,6 +13,7 @@ type Cmd struct {
 
 const (
 	typeCmdWithArgv = "cmd_with_argv"
+	typeHear        = "hear"
 )
 
 var commands = make([]*Cmd, 0)
@@ -22,6 +23,8 @@ func Init(bot *bot.Bot) {
 		switch c.Type {
 		case typeCmdWithArgv:
 			bot.CommandWithArgv(c.Pattern, c.Description, c.Handler)
+		case typeHear:
+			bot.Hear(c.Pattern, c.Handler)
 		default:
 			bot.Command(c.Pattern, c.Description, c.Handler)
 		}
@@ -36,9 +39,16 @@ func AddCommandWithArgv(pattern, description string, handler func(*bot.Event)) {
 	commands = append(commands, &Cmd{Type: typeCmdWithArgv, Pattern: pattern, Description: description, Handler: handler})
 }
 
+func Hear(pattern string, handler func(*bot.Event)) {
+	commands = append(commands, &Cmd{Type: typeHear, Pattern: pattern})
+}
+
 func ShowHelp() string {
 	help := ""
 	for _, c := range commands {
+		if c.Type == typeHear {
+			continue
+		}
 		if c.Description != "" {
 			help += c.Pattern + ": " + c.Description + "\n"
 		}
