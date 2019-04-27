0cef2bfd0b5870c8fde15f3c048ba0282cac8eca
Fumihiro Itoh <fmhrit@gmail.com>
2019-04-27 23:22:17 +0900

    fix handling reaction added event

--- bot/event_handler.go
+++ bot/event_handler.go
@@ -169,12 +169,16 @@ func (eventHandler *EventHandler) Handle(event *Event, async bool) {
 				eventHandler.commandCallback(command, event, async)
 			}
 		case ReactionAddedEvent:
-			if time.Now().Sub(command.createdAt) >= ReactionExpire {
+			if !command.createdAt.IsZero() && time.Now().Sub(command.createdAt) >= ReactionExpire {
 				go eventHandler.RemoveRequireReaction(command.messageId, command.reaction)
 				continue
 			}
 			if event.EventId() == command.messageId && event.User.Id == command.user && event.Reaction == command.reaction {
 				go eventHandler.RemoveRequireReaction(event.EventId(), event.Reaction)
+				eventHandler.commandCallback(command, event, async)
+				return
+			}
+			if event.Reaction == command.reaction {
 				eventHandler.commandCallback(command, event, async)
 			}
 		}
